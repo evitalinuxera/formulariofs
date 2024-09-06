6d09858afa22698682f4baffad778b7309e97fe7
Evitalinuxera <riverplatense@gmail.com>
2024-09-06 13:27:25 -0300

    Header y footer como componentes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer () {
+
+return (
+    <>
+        <h2>Acá va el footer</h2>
+    </>
+)
+
+} 
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header () {
+
+return (
+    <h1>Acá va el Header</h1>
+)    
+}
+
